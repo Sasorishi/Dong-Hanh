f42298ac8d79cc89dfe2cad8dca7fb4d0620a276
alo <a.on@thecodingmachine.com>
2024-01-30 12:58:10 +0100

    Fix visual and minors changes ux

--- assets/js/components/homepage/ContactComponent.js
+++ assets/js/components/homepage/ContactComponent.js
@@ -60,27 +60,6 @@ const ContactSection = ({ onError }) => {
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
                 <label
-                  htmlFor="firstName"
-                  className="block text-sm font-semibold leading-6 text-darkblue"
-                >
-                  First name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="firstName"
-                    id="firstName"
-                    autoComplete="given-name"
-                    placeholder="Thanh Việt"
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-darkblue shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    required
-                  />
-                </div>
-              </div>
-              <div>
-                <label
                   htmlFor="lastName"
                   className="block text-sm font-semibold leading-6 text-darkblue"
                 >
@@ -94,6 +73,27 @@ const ContactSection = ({ onError }) => {
                     autoComplete="family-name"
                     placeholder="Nguyễn"
                     value={formData.lastName}
+                    onChange={handleInputChange}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-darkblue shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    required
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm font-semibold leading-6 text-darkblue"
+                >
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    autoComplete="given-name"
+                    placeholder="Thanh Việt"
+                    value={formData.firstName}
                     onChange={handleInputChange}
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-darkblue shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     required
