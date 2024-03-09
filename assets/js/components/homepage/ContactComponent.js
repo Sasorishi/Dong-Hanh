05c350c295f78076d7db603ad7e89869ad6f5c99
Sasorishi <on_alban@yahoo.fr>
2024-03-09 05:51:10 +0100

    Fix style and pages

--- assets/js/components/homepage/ContactComponent.js
+++ assets/js/components/homepage/ContactComponent.js
@@ -203,19 +203,20 @@ const ContactSection = ({ onError }) => {
                     <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-whitesmoke after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-cognac"></div>
                   </label>
                 </div>
-                <label
+                <p
                   className="text-sm leading-6 text-gray-600"
                   id="switch-1-label"
                 >
                   By selecting this, you agree to our{" "}
-                  <span
-                    className="font-semibold text-indigo-600 hover:underline"
-                    onClick={handleOpenDefaultModal}
+                  <a
+                    href="/privacy_policy"
+                    className="font-semibold text-cognac hover:underline"
+                    // onClick={handleOpenDefaultModal}
                   >
                     privacy&nbsp;policy
-                  </span>
+                  </a>
                   .
-                </label>
+                </p>
               </div>
             </div>
           </div>
