224cf15713056f8a2d5a3736e211b58499dfa471
Sasorishi <on_alban@yahoo.fr>
2024-06-16 02:44:53 +0200

    Add contraints on year and features

--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -10,6 +10,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class EventCrudController extends AbstractCrudController
 {
@@ -39,9 +42,16 @@ class EventCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         $fields = parent::configureFields($pageName);
+        $fields[] = NumberField::new('year', 'Year')->setFormTypeOptions(['constraints' => [
+            new Assert\Positive(),
+        ]]);
         $fields[] = AssociationField::new('eventCategory', 'Category');
         $fields[] = ArrayField::new('images', 'Images');
-        $fields[] = ArrayField::new('features', 'Features');
+        $fields[] = ArrayField::new('features', 'Features')->setFormTypeOptions(['constraints' => [
+            new Assert\Count([
+                'max' => 4,
+            ]),
+        ]]);
         $fields[] = ArrayField::new('checklist', 'Checklist');
         $fields[] = CollectionField::new('getCompletedParticipants', "Participants")->onlyOnDetail()->setTemplatePath('admin/fields/participants.html.twig');
         return $fields;
--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: EventRepository::class)]
 class Event
@@ -53,6 +54,9 @@ class Event
     private ?\DateTimeInterface $dateEnd = null;
 
     #[ORM\Column(type: Types::JSON, nullable: true)]
+    #[Assert\Count(
+        max: 4,
+    )]
     private ?array $features = null;
 
     #[ORM\OneToMany(mappedBy: 'event', targetEntity: Participant::class)]
