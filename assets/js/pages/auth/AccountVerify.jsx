66e6f41b6dc735e39b50f50c4c76abcac703d9f0
MatteoBaldinetti <matteo.baldinetti@edu.univ-eiffel.fr>
2024-07-10 10:07:18 +0200

    Add code and expiration date verification

--- assets/js/pages/auth/AccountVerify.jsx
+++ assets/js/pages/auth/AccountVerify.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Flex, Input, Typography } from 'antd';
+import axios from "axios";
 
 const AccountVerify = () => {
+    const [userId, setUserId] = useState(null);
+    const [code, setCode] = useState(null);
+    const [expiredAt, setExpiredAt] = useState(null);
+    const [error, setError] = useState(null);
+
+    var id = window.location.href.split('/');
+    id = id[id.length - 1];
+
+    const verifyCode = async (text, code, expiredAt) => {
+        if (text == code) {
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            if (month < 10) {
+                month = "0" + month;
+            }
+            var day = date.getDate();
+            if (day < 10) {
+                day = "0" + day;
+            }
+            var hours = date.getHours();
+            if (hours < 10) {
+                hours = "0" + hours;
+            }
+            var minutes = date.getMinutes();
+            if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+            var seconds = date.getSeconds();
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+            var currentDate = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds + ".000000";
+            console.log(
+                expiredAt.date, currentDate
+            )
+            if (expiredAt.date >= currentDate) {
+                await axios.post(`/api/auth/account-verify/database/${id}`);
+                window.location.href = "/response/success/signup";
+            } else {
+                console.log(0);
+            }
+        } else {
+            console.log(0);
+        }
+    }
+
+    const handleAccountVerify = async () => {
+        const response = await axios.post(`/api/auth/account-verify/${id}`);
+        if (response.data.success !== false) {
+            setUserId(response.data.userId);
+            setCode(response.data.code);
+            setExpiredAt(response.data.expiredAt);
+        } else {
+            setError(`Error : ${response.data.message}.`);
+        }
+    }
 
     const onChange = (text) => {
-        console.log(text);
+        handleAccountVerify();
+        verifyCode(text, code, expiredAt)
     };
 
     const sharedProps = {
