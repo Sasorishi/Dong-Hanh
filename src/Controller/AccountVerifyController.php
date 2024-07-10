14f779881956bd717c15f5fb289630eedbb457e5
MatteoBaldinetti <matteo.baldinetti@edu.univ-eiffel.fr>
2024-07-10 10:37:10 +0200

    add resend email function

--- src/Controller/AccountVerifyController.php
+++ src/Controller/AccountVerifyController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\AccountCodeVerifyRepository;
 use App\Repository\UserRepository;
+use App\Service\AccountVerifyService;
 use App\Service\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -16,13 +17,15 @@ class AccountVerifyController extends AbstractController
     private $accountCodeVerifyRepository;
     private $userRepository;
     private $mailerService;
+    private $accountVerifyService;
     private $params;
 
-    public function __construct(AccountCodeVerifyRepository $accountCodeVerifyRepository, UserRepository $userRepository, MailerService $mailerService, ParameterBagInterface $params)
+    public function __construct(AccountCodeVerifyRepository $accountCodeVerifyRepository, UserRepository $userRepository, MailerService $mailerService, ParameterBagInterface $params, AccountVerifyService $accountVerifyService)
     {
         $this->accountCodeVerifyRepository = $accountCodeVerifyRepository;
         $this->userRepository = $userRepository;
         $this->mailerService = $mailerService;
+        $this->accountVerifyService = $accountVerifyService;
         $this->params = $params;
     }
 
@@ -38,6 +41,29 @@ class AccountVerifyController extends AbstractController
     public function accountVerify($id): JsonResponse
     {
         $user = $this->accountCodeVerifyRepository->findOneBy(['user' => $id]);
+        return new JsonResponse(['success' => true, 'message' => 'Signin successful', 'userId' => $user->getUser()->getId(), 'code' => $user->getCode(), 'expiredAt' => $user->getExpiredAt()]);
+    }
+
+    #[Route('/api/auth/account-verify/resend/{id}', name: 'api_auth_account-verify', methods: ['POST'])]
+    public function resentEmail($id): JsonResponse
+    {
+        $mail = $this->params->get("app.mail_address");
+        $user = $this->accountCodeVerifyRepository->findOneBy(['user' => $id]);
+        $newCode = $this->accountVerifyService->codeGenerator();
+        $newCode = $this->accountVerifyService->arrayToString($newCode);
+        $user->setCode($newCode);
+        $dateTime = new \DateTime();
+        $dateTime->modify("+10 minutes");
+        $user->setExpiredAt($dateTime);
+        $this->accountCodeVerifyRepository->save($user, true);
+        $userData = $this->userRepository->findOneBy(["id" => $id]);
+        $context = ([
+            'user_id' => $userData->getId(),
+            'user_email' => $userData->getEmail(),
+            'code' => $userData,
+            'current_year' => new \DateTime('Y')
+        ]);
+        $this->mailerService->sendTemplateEmail($mail, $userData->getEmail(), "Verify your account", 'emails/verification_code.html.twig', $context);
         return new JsonResponse(['success' => true, 'message' => 'Signin successful', 'userId' => $user->getUser()->getId(), 'code' => $user->getCode(), 'expiredAt' => $user->getExpiredAt()]);
     }
 
