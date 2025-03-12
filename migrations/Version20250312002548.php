0cd426dab9fc05e4d5d0066e5c2f6d3966717b18
Sasorishi <on_alban@yahoo.fr>
2025-03-12 01:31:24 +0100

    Add field public and online

---
+++ migrations/Version20250312002548.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250312002548 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event ADD public TINYINT(1) NOT NULL DEFAULT 0');
+        $this->addSql('ALTER TABLE event ADD online TINYINT(1) NOT NULL DEFAULT 0');
+        $this->addSql('ALTER TABLE event CHANGE place place VARCHAR(255) DEFAULT NULL, CHANGE location location VARCHAR(255) DEFAULT NULL, CHANGE currency currency VARCHAR(12) DEFAULT NULL, CHANGE public public TINYINT(1) NOT NULL, CHANGE online online TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event DROP public');
+        $this->addSql('ALTER TABLE event DROP online');
+        $this->addSql('ALTER TABLE event CHANGE place place VARCHAR(255) NOT NULL, CHANGE location location VARCHAR(255) NOT NULL, CHANGE currency currency VARCHAR(12) NOT NULL, CHANGE public public TINYINT(1) DEFAULT 0 NOT NULL, CHANGE online online TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+}
