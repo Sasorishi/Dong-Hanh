0e7eddd8e5c77eaf6707b50fd2af4a545a53c364
Sasorishi <on_alban@yahoo.fr>
2025-06-30 20:33:09 +0200

    feat: add usage limits, email template for staff and free pass code

--- src/Controller/Api/DiscountVoucherController.php
+++ src/Controller/Api/DiscountVoucherController.php
@@ -6,34 +6,44 @@ use App\Repository\DiscountVoucherRepository;
 use App\Repository\DiscountVoucherUsageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DiscountVoucherController extends AbstractController
 {
     #[Route('/api/discount/{code}', methods: 'GET')]
-    public function getDiscountVoucher(DiscountVoucherRepository $discountVoucherRepository, DiscountVoucherUsageRepository $discountVoucherUsageRepository, string $code): JsonResponse
+    public function getDiscountVoucher(Request $request, DiscountVoucherRepository $discountVoucherRepository, DiscountVoucherUsageRepository $discountVoucherUsageRepository, string $code): JsonResponse
     {
-        $discount = $discountVoucherRepository->findOneBy(['code' => $code]);
-        $discountVoucherUsage = $discountVoucherUsageRepository->findOneBy(['DiscountVoucher' => $discount->getId()]);
-        
-        if (!$discount) {
-            return new JsonResponse(Response::HTTP_NOT_FOUND);
-        }
-        
-        if (!$discount->isUsable()) {
-            return new JsonResponse(['message' =>  "Code is not usable"], Response::HTTP_BAD_REQUEST);
-        }
-        
-        if (!$discountVoucherUsage) {
-            return new JsonResponse(['message' =>  "Code can't be use for this event"], Response::HTTP_BAD_REQUEST);
+        try {
+            $eventId = $request->query->get('eventId');
+            $discount = $discountVoucherRepository->findOneBy(['code' => $code]);
+            
+            if (!$discount) {
+                return new JsonResponse(Response::HTTP_NOT_FOUND);
+            }
+            
+            if (!$discount->isUsable()) {
+                return new JsonResponse(['message' =>  "Code is not usable"], Response::HTTP_BAD_REQUEST);
+            }
+            
+            if ($discount->getEvent()->getId() != $eventId) {
+                return new JsonResponse(['message' =>  "Code can't be use for this event"], Response::HTTP_BAD_REQUEST);
+            }
+
+            if ($discount->getUsageLimits() !== null && $discount->getDiscountVoucherUsages()->count() >= $discount->getUsageLimits()) {
+                return new JsonResponse(['message' =>  "Code can't be use anymore"], Response::HTTP_BAD_REQUEST);
+            }
+
+            $data = [
+                'id' => $discount->getId(),
+                'code' => $discount->getCode(),
+                'discount' => $discount->getDiscountPercentage()
+            ];
+        } catch (\Throwable $th) {
+            throw $th;
         }
 
-        $data = [
-            'id' => $discount->getId(),
-            'code' => $discount->getCode(),
-            'discount' => $discount->getDiscountPercentage()
-        ];
 
         return new JsonResponse(['voucher' =>  $data], Response::HTTP_OK);
     }
